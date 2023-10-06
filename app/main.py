194d85fd6b82ffe043f9edf2debaa9255ea880ef
BeanGirlThing <ampstead@amazon.com>
2023-10-06 07:02:17 +0100

    Hopefully fix CRSF issue

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ from sessionHandler import SessionHandler
 app = Flask("FerretTickets")
 app.secret_key = secrets.token_urlsafe(16)
 app.config.update(
-    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_SECURE=False,
     SESSION_COOKIE_HTTPONLY=True,
     SESSION_COOKIE_SAMESITE='Lax',
 )
